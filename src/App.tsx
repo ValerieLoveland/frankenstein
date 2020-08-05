47036c66fdd6df2bda090da1cfac6c226e267442
Valerie <valerieloveland@gmail.com>
2020-08-04 21:38:16 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+function App(this: any) {
+  const [unitsF, setUnitsF] = useState(true);
+  const [temp, setTemp] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Temperature Converter</header>
+
+      <div>
+        <form className="temp-text" onSubmit={() => false}>
+          <div>
+            <label> Convert Farenheit</label>
+            <input
+              onChange={(event) => {
+                setTemp(parseInt(event.currentTarget.value));
+                setUnitsF(true);
+                console.log(temp, unitsF);
+              }}
+            />
+          </div>
+
+          <label>Convert Celsius</label>
+          <input
+            onChange={(event) => {
+              setTemp(parseInt(event.target.value));
+              setUnitsF(false);
+              console.log(temp, unitsF);
+            }}
+          />
+        </form>
+        <div>{temp}</div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//set up state
+//hook this into the click event

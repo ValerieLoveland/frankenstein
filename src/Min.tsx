93e87741f1db5e32c1c90a45b0a568154bb1ba1c
Valerie <valerieloveland@gmail.com>
2020-09-23 09:04:48 -0400

    input fields working

--- src/Min.tsx
+++ src/Min.tsx
@@ -1,25 +1,51 @@
-import React from "react";
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
 export const Min = () => {
-  // const [minInput, setMinInput] = useState<MinUserInput>({
-  //   input1: 0,
-  //   input2: 0,
-  // });
+  const [minInput1, setMinInput1] = useState({
+    input1: 0,
+  });
+  const [minInput2, setMinInput2] = useState({
+    input2: 0,
+  });
 
-  return <div>Minimum Number</div>;
+  return (
+    <>
+      <form onSubmit={() => false}>
+        <header>Smallest Number</header>
+
+        <label>First Number: </label>
+        <input
+          onChange={(event) => {
+            setMinInput1({
+              input1: parseInt(event.currentTarget.value),
+            });
+          }}
+        />
+
+        <div>
+          <label>Second Number: </label>
+          <input
+            onChange={(event) => {
+              setMinInput2({
+                input2: parseInt(event.currentTarget.value),
+              });
+            }}
+          />
+        </div>
+      </form>
+      <div>{JSON.stringify(minInput1)}</div>
+      <div>{JSON.stringify(minInput2)}</div>
+    </>
+  );
 };
 
-// const determineMin=(input1:number)=>{
-
-//     let minNum = 0;
-//     if (input1 < input2) {
-//       minNum = input1;
-//     } else {
-//       minNum = input2;
-//     }
-//     console.log(minNum);
-//     return minNum;
-//   }
-
-// type MinUserInput = { input1: number; input2: number };
+const determineMin = (input1: number, input2: number) => {
+  let minNum = 0;
+  if (input1 < input2) {
+    minNum = input1;
+  } else {
+    minNum = input2;
+  }
+  console.log(minNum);
+  return minNum;
+};

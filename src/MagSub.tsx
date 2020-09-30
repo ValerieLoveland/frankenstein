0c42802ab3b0a863fd0a53d0832fe8ef25bf1100
Valerie <valerieloveland@gmail.com>
2020-09-29 23:24:45 -0400

    before I start the object or table

--- src/MagSub.tsx
+++ src/MagSub.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const MagSub = () => {
   return (
     <>
-      <h1>Magazine Submissions</h1>
+      <h1>Magazines to Query</h1>
       <div className="ul-style form">
         <div className="sub">
           <div>Poem</div>
@@ -11,8 +11,13 @@ export const MagSub = () => {
           <div>Date</div>
           <div>Submitted</div>
         </div>
-        <li>{submission("Frosties", "Poetry Magazine", "7/20/2020")}</li>
-        <li>{submission("Slap Bracelets", "Kenyon Review", "08/15/2020")}</li>
+        <ul>
+          <li>{submission("Frosties", "Poetry Magazine", "7/20/2020")}</li>
+          <li>{submission("Slap Bracelets", "Kenyon Review", "08/15/2020")}</li>
+          <li>
+            {submission("Grabber", "American Poetry Review", "09/15/2020")}
+          </li>
+        </ul>
       </div>
     </>
   );
@@ -29,10 +34,18 @@ const submission = function (
 
   let daysOut = calculateDays(currentDate, alteredStartDate);
 
+  let subWithQuery = ` "${poem}" ${magazine} ${startDate}
+    
+    Query ${magazine} about "${poem}." It has been out ${daysOut} days.`;
+
+  let subNoQuery = ` "${poem}" ${magazine} ${startDate} ${daysOut}`;
+
   if (daysOut > 30) {
-    console.log(`query ${magazine} about ${poem}`);
+    console.log(`Query ${magazine} about ${poem}`);
+    return subWithQuery;
+  } else {
+    return subNoQuery;
   }
-  return `query ${magazine} about ${poem}`;
 };
 
 const calculateDays = (
@@ -42,4 +55,11 @@ const calculateDays = (
   let days = Math.floor((currentDate - alteredStartDate) / 86400000);
   console.log(days);
   return days;
+};
+
+export type Submission = {
+  poem: string;
+  magazine: string;
+  startDate: string;
+  daysOut: number;
 };
